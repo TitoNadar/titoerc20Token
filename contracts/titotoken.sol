d7153ead9b6944647396b24f972e33cc302c9efa
TitoNadar <titonadar06@gmail.com>
2018-07-10 13:40:07 +0530

    transfer ownership test cleared

--- contracts/titotoken.sol
+++ contracts/titotoken.sol
@@ -1,7 +1,10 @@
 pragma solidity ^0.4.2;
 
 contract titotoken{
-
+   
+    string public name="titotoken";
+    string public symbol="TiTo";
+    string public standard="titotoken v1.0";
     uint256 public totalSupply;
     mapping(address=>uint256) public balanceOf;
     
@@ -11,5 +14,21 @@ contract titotoken{
         totalSupply = _initialSupply;
         
     }
+    
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
 
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
 }
--- test/titotoken.js
+++ test/titotoken.js
@@ -2,6 +2,21 @@ var titotoken=artifacts.require('./titotoken.sol');
 
 contract('titotoken',function(accounts){
     var tokeninstance;
+    it('initializes the contract with the correct values', function() {
+        return titotoken.deployed().then(function(instance) {
+          tokenInstance = instance;
+          return tokenInstance.name();
+        }).then(function(name) {
+          assert.equal(name, 'titotoken', 'has the correct name');
+          return tokenInstance.symbol();
+        }).then(function(symbol) {
+          assert.equal(symbol, 'TiTo', 'has the correct symbol');
+          return tokenInstance.standard();
+        }).then(function(standard) {
+          assert.equal(standard, 'titotoken v1.0', 'has the correct standard');
+        });
+    });
+
     it('sets total supply',function(){
         return titotoken.deployed().then(function(instance){
             tokeninstance=instance;
@@ -12,5 +27,31 @@ contract('titotoken',function(accounts){
         }).then(function(adminBalance){
             assert.equal(adminBalance.toNumber(),1000000,'admin')
         })
+    });
+
+    it('transfers token ownership', function() {
+        return titotoken.deployed().then(function(instance) {
+          tokenInstance = instance;
+          // Test `require` statement first by transferring something larger than the sender's balance
+          return tokenInstance.transfer.call(accounts[1], 99999999999999999999999);
+        }).then(assert.fail).catch(function(error) {
+          assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
+          return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
+        }).then(function(success) {
+          assert.equal(success, true, 'it returns true');
+          return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
+        }).then(function(receipt) {
+          assert.equal(receipt.logs.length, 1, 'triggers one event');
+          assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+          assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
+          assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to');
+          assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+          return tokenInstance.balanceOf(accounts[1]);
+        }).then(function(balance) {
+          assert.equal(balance.toNumber(), 250000, 'adds the amount to the receiving account');
+          return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(balance) {
+          assert.equal(balance.toNumber(), 750000, 'deducts the amount from the sending account');
+        });
     });
 })
